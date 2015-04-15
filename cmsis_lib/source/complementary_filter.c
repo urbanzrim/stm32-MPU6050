d4b14b6ef97c7af4f3e212ba1804dbbe6f2e2c75
Urban Zrim <urban.zrim@gmail.com>
2015-04-15 13:01:31 +0200

    License headers added
    MIT license is added in headers

---
+++ cmsis_lib/source/complementary_filter.c
@@ -0,0 +1,9 @@
+/**
+ * @file complementary_filter.c
+ * @brief Source file for complementary filter function
+ *
+ * @author Urban Zrim
+ * @date 13.4.2015
+ *
+ */
+
--- main.c
+++ main.c
@@ -1,8 +1,32 @@
-/*
- * main.c
+/**
+ * @file main.c
+ * @brief Main program for application
  *
- *  Created on: 7. apr. 2015
- *      Author: Urban Zrim
+ * @author Urban Zrim
+ * @date 7.4.2015
+ *
+ *
+ *  --------------------------------------------------------------------------------
+ *  Copyright (c) 2015, Urban Zrim
+ *
+ *	Permission is hereby granted, free of charge, to any person obtaining a copy
+ *	of this software and associated documentation files (the "Software"), to deal
+ *	in the Software without restriction, including without limitation the rights
+ *	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *	copies of the Software, and to permit persons to whom the Software is
+ *	furnished to do so, subject to the following conditions:
+ *
+ *	The above copyright notice and this permission notice shall be included in
+ *	all copies or substantial portions of the Software.
+ *
+ *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *	THE SOFTWARE.
+ *  --------------------------------------------------------------------------------
  */
 
 #include <stdio.h>
@@ -19,25 +43,20 @@ int main(void)
 	float accel_ydata;
 	float accel_zdata;
 	gpio_init();
-	//tim_init();
+	//tim_init(); // Timers are set in the dboardsetup.h for quadcopter
 	uart_init();
 	i2c_init();
-	int16_t str_gyro[200];
-	int16_t str_accel[200];
-	int i = 0;
+	uint8_t str[25];
 
 	err = MPU6050_Initialization();
 
     while(1)
     {
+    	/* Read data from the sensor */
+		err = MPU6050_Get_Gyro_Data(&gyro_xdata, &gyro_ydata, &gyro_zdata);
+		err = MPU6050_Get_Accel_Data(&accel_xdata, &accel_ydata, &accel_zdata);
 
-    	while(i<200){
-			err = MPU6050_Get_Gyro_Data(&gyro_xdata, &gyro_ydata, &gyro_zdata);
-			err = MPU6050_Get_Accel_Data(&accel_xdata, &accel_ydata, &accel_zdata);
-			str_gyro[i] = gyro_xdata;
-			str_accel[i] = accel_xdata;
-			i++;
-    	}
-
+		sprintf(str, "%f", gyro_xdata);
+		printf("gyroX: %s", gyro_xdata);
     }
 }
